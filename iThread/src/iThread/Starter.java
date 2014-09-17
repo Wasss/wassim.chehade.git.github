716c0e853fecfcbe6e56d5942fc2f463b420787d
wassim.chehade@gmail.com <wassim.chehade@gmail.com>
2014-09-17 14:06:28 +0300

    i added this method

--- iThread/src/iThread/Starter.java
+++ iThread/src/iThread/Starter.java
@@ -1,11 +1,12 @@
 package iThread;
 
-//this is the starter class
+//this is the starter class i will modified it
 public class Starter {
 
 	public static void main(String[] args) {
 		TrajectoryController controller = new TrajectoryController();
 		controller.init();
+		controller.equals("");
 	}
 
 }
